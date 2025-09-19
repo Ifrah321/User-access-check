a21f5b1c11bb5e5924da6e5c2e29c50b5a24b2fe
Ifrah <s235653@dtu.dk>
2025-09-19 16:27:45 +0200

    Aflevering 3

---
+++ aflevering 3/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Brugernavn: ");
+string username = Console.ReadLine();
+
+Console.Write("Kodeord: ");
+string password = Console.ReadLine();
+
+Console.Write("UserId (heltal): ");
+uint userId = uint.Parse(Console.ReadLine());
+
+bool userIsAdmin = userId > 65536;
+
+bool usernameValid = username.Length >= 3;
+
+bool hasSpecial = password.Contains("$") || password.Contains("|") || password.Contains("@");
+
+bool passwordValid = false;
+if (userIsAdmin && password.Length >= 20 && hasSpecial)
+{
+    passwordValid = true;
+}
+else if (!userIsAdmin && password.Length >= 16 && hasSpecial)
+{
+    passwordValid = true;
+}
+
+Console.WriteLine();
+Console.WriteLine("userIsAdmin: " + userIsAdmin);
+Console.WriteLine("usernameValid: " + usernameValid);
+Console.WriteLine("passwordValid: " + passwordValid);
